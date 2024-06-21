d5be91a38086ccbbbc97f1f7c7ee54d7daf7e1ac
JoseDCaballero <22090601@alumno.utmetropolitana.edu.mx>
2024-06-21 13:55:33 -0600

    imagen

--- src/pages/CategoryPage.vue
+++ src/pages/CategoryPage.vue
@@ -1,41 +1,63 @@
 <template>
     <div>
-        <h1>Estas son las categorias de nuestros productos</h1>
-        <div class="categories">
-            <CategoryCard v-for="category in categories" :key="category.name" :category="category"
-                @select-category="selectCategory" />
-        </div>
+      <h1>Estas son las categorias de nuestros productos</h1>
+      <div v-if="isLoading" class="loading-container">
+        <img src="../images/loading.gif" alt="Cargando" class="loading-gif"/>
+        <p>Cargando...</p>
+      </div>
+      <div v-else class="categories">
+        <CategoryCard v-for="category in categories" :key="category.name" :category="category"
+            @select-category="selectCategory" />
+      </div>
     </div>
-</template>
-
-<script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
-import CategoryCard from '../components/CategoryCard.vue';
-import { useRouter } from 'vue-router';
-
-const categories = ref([]);
-const router = useRouter();
-
-async function fetchCategories() {
-    try {
-        const response = await axios.get(import.meta.env.VITE_API_TELASEMANUEL+'/categories/');
-        categories.value = response.data;
-    } catch (error) {
-        console.error('Error fetching categories:', error);
-    }
-}
-
-function selectCategory(categoryName) {
-    router.push('/categories/'+ categoryName );
-}
-
-onMounted(fetchCategories);
-</script>
-
-<style scoped>
-.categories {
-    display: flex;
-    flex-wrap: wrap;
-}
-</style>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
+  import CategoryCard from '../components/CategoryCard.vue';
+  import { useRouter } from 'vue-router';
+  
+  const categories = ref([]);
+  const isLoading = ref(true);
+  const router = useRouter();
+  
+  async function fetchCategories() {
+      isLoading.value = true;
+      try {
+          const response = await axios.get(import.meta.env.VITE_API_TELASEMANUEL + '/categories/');
+          categories.value = response.data;
+      } catch (error) {
+          console.error('Error fetching categories:', error);
+      } finally {
+          isLoading.value = false;
+      }
+  }
+  
+  function selectCategory(categoryName) {
+      router.push('/categories/' + categoryName);
+  }
+  
+  onMounted(fetchCategories);
+  </script>
+  
+  <style scoped>
+  .categories {
+      display: flex;
+      flex-wrap: wrap;
+  }
+  
+  .loading-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin-top: 20px;
+  }
+  
+  .loading-gif {
+      width: 500px;
+      height: 450px;
+  }
+  </style>
+  
--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <h1>{{ categoryName }}</h1>
-        <ProductList :products="products" />
+        <div v-if="isLoading" class="loading-container">
+            <img src="../images/loading.gif" alt="Cargando" class="loading-gif" />
+            <p>Cargando...</p>
+        </div>
+        <ProductList v-else :products="products" />
     </div>
 </template>
 
@@ -14,15 +18,19 @@ import ProductList from '../components/ProductList.vue';
 const route = useRoute();
 const categoryName = ref(route.params.categoryName);
 const products = ref([]);
+const isLoading = ref(true);
 
 async function fetchProducts() {
+    isLoading.value = true;
     try {
-        const response = await axios.get(import.meta.env.VITE_API_TELASEMANUEL+`/products/`, {
+        const response = await axios.get(import.meta.env.VITE_API_TELASEMANUEL + `/products/`, {
             params: { categoryName: categoryName.value }
         });
         products.value = response.data;
     } catch (error) {
         console.error('Error fetching products:', error);
+    } finally {
+        isLoading.value = false;
     }
 }
 
@@ -33,3 +41,18 @@ watch(() => route.params.categoryName, (newCategoryName) => {
 
 onMounted(fetchProducts);
 </script>
+
+<style>
+.loading-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+}
+
+.loading-gif {
+    width: 500px;
+    height: 450px;
+}
+</style>
