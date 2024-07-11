1a4f1d9c49a02dad8795d3ecf3b1cff3b4482863
JoseDCaballero <133797263+JoseDCaballero@users.noreply.github.com>
2024-07-11 15:11:42 -0500

    Update HomePage.vue

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,7 +3,7 @@
     <ModalCart />
     <h1>Telas Emanuel</h1>
       <div id="marco">
-        <img src="LOGO">
+        <img :src="LOGO">
       <div id="logo">
         <h1 style="color: #000;">LONAS Y VINILES</h1>
         <h1 style="color: #000;">TELAS PARA TAPICERIA Y MAS...</h1>
